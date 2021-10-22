baddb2a975d4837b2fca17a1b1563f755289cecb
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-10-21 19:45:22 -0700

    adds styling to categories and updates data model

--- src/data.js
+++ src/data.js
@@ -25,18 +25,18 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "SHIRT STYLE!",
+    img: "https://i.imgur.com/X8txBxE.jpg",
+    title: "HANDBAG ADDITION",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    title: "LOUNGEWEAR LOVE",
+    img: "https://i.imgur.com/LEobL6J.jpg",
+    title: "CRAFTED BY HAND",
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-    title: "LIGHT JACKETS",
+    img: "https://i.imgur.com/vWKY2rk.jpeg",
+    title: "ELEGANT & LOVELY",
   },
 ];
 
